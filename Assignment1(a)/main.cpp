89c654c8879de2370259fa95cab82b46471408d3
Divyansha <divyanshaarora1@gmail.com>
2023-01-06 13:22:50 +0530

    Initial Commit

---
+++ Assignment1(a)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assignment1(a)
+//
+//  Created by Divyansha Arora on 06/01/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
